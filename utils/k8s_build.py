4334316f16acf7a901e8c1fd46bbeeabbdd3035d
jellegroot <jelle.groot@hotmail.nl>
2025-12-11 17:17:58 +0000

    ssh

--- utils/k8s_build.py
+++ utils/k8s_build.py
@@ -9,6 +9,7 @@ import base64
 from .k8s_manage_objects import get_template, deploy_object
 from .k8s_database import get_config
 from .k8s_client import get_k8s_client
+import re
 
 
 
@@ -35,14 +36,21 @@ def build_from_repository(challenge_name, repository):
     git_user = base64.b64encode(str(config.git_user).encode('ascii')).decode('ascii')
     git_password = base64.b64encode(str(config.git_credential).encode('ascii')).decode('ascii')
 
+
+    repo_name = re.sub(r'\.git$', '', repository.split('/')[-1])
+    
     template = get_template('build')
-    options = { 'challenge_name': challenge_name,
-                'challenge_repo': repository,
-                'registry_namespace': config.registry_namespace,
-                'https_domain_name': config.https_domain_name,
-                'git_credential': git_password,
-                'git_user': git_user,
-                'registry_data': registry_data}
+    options = { 
+        'challenge_name': challenge_name,
+        'challenge_repo': repository,
+        'registry_namespace': config.registry_namespace,
+        'https_domain_name': config.https_domain_name,
+        'git_credential': git_password,
+        'git_user': git_user,
+        'registry_data': registry_data,
+        'repo_name': repo_name
+    }
+
 
     print("Building challenge...")
     if deploy_object(get_k8s_client(), template, options):
